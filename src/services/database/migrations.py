f4757ac5f4c3b8aa5aa2a096406a7d564ef3f4f1
KOSASIH <kosasihg88@gmail.com>
2024-11-08 19:45:28 +0700

    Create migrations.py

---
+++ src/services/database/migrations.py
@@ -0,0 +1,12 @@
+# database/migrations.py
+
+from flask_migrate import MigrateCommand
+from flask_script import Manager
+from app import app
+from db import db
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
